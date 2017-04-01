58f92832cdf440edad06314f46a0fdfa402584f2
GTAddict <soregtaddict@gmail.com>
2017-04-01 00:42:32 -0400

    Put GameClock and GameTime in the GameEngine.Library namespace

--- source/Library.Desktop/GameClock.h
+++ source/Library.Desktop/GameClock.h
@@ -3,27 +3,30 @@
 #include <exception>
 #include <chrono>
 
-namespace Library
+namespace GameEngine
 {
-    class GameTime;
+	namespace Library
+	{
+		class GameTime;
 
-    class GameClock
-    {
-    public:
-        GameClock();
-		GameClock(const GameClock& rhs) = delete;
-		GameClock& operator=(const GameClock& rhs) = delete;
+		class GameClock
+		{
+		public:
+			GameClock();
+			GameClock(const GameClock& rhs) = delete;
+			GameClock& operator=(const GameClock& rhs) = delete;
 
-		const std::chrono::high_resolution_clock::time_point& StartTime() const;
-		const std::chrono::high_resolution_clock::time_point& CurrentTime() const;
-		const std::chrono::high_resolution_clock::time_point& LastTime() const;
+			const std::chrono::high_resolution_clock::time_point& StartTime() const;
+			const std::chrono::high_resolution_clock::time_point& CurrentTime() const;
+			const std::chrono::high_resolution_clock::time_point& LastTime() const;
 
-        void Reset();
-        void UpdateGameTime(GameTime& gameTime);
+			void Reset();
+			void UpdateGameTime(GameTime& gameTime);
 
-    private:
-        std::chrono::high_resolution_clock::time_point mStartTime;
-		std::chrono::high_resolution_clock::time_point mCurrentTime;
-		std::chrono::high_resolution_clock::time_point mLastTime;
-    };
+		private:
+			std::chrono::high_resolution_clock::time_point mStartTime;
+			std::chrono::high_resolution_clock::time_point mCurrentTime;
+			std::chrono::high_resolution_clock::time_point mLastTime;
+		};
+	}
 }
--- source/Library.Desktop/GameTime.h
+++ source/Library.Desktop/GameTime.h
@@ -3,25 +3,28 @@
 #include <chrono>
 #include "GameClock.h"
 
-namespace Library
+namespace GameEngine
 {
-	class GameTime final
+	namespace Library
 	{
-	public:
-		GameTime();
+		class GameTime final
+		{
+		public:
+			GameTime();
 
-		const std::chrono::high_resolution_clock::time_point& CurrentTime() const;
-		void SetCurrentTime(const std::chrono::high_resolution_clock::time_point& currentTime);
-		
-		const std::chrono::milliseconds& TotalGameTime() const;
-		void SetTotalGameTime(const std::chrono::milliseconds& totalGameTime);
+			const std::chrono::high_resolution_clock::time_point& CurrentTime() const;
+			void SetCurrentTime(const std::chrono::high_resolution_clock::time_point& currentTime);
 
-		const std::chrono::milliseconds& ElapsedGameTime() const;		
-		void SetElapsedGameTime(const std::chrono::milliseconds& elapsedGameTime);
+			const std::chrono::milliseconds& TotalGameTime() const;
+			void SetTotalGameTime(const std::chrono::milliseconds& totalGameTime);
 
-	private:
-		std::chrono::high_resolution_clock::time_point mCurrentTime;
-		std::chrono::milliseconds mTotalGameTime;
-		std::chrono::milliseconds mElapsedGameTime;
-	};
+			const std::chrono::milliseconds& ElapsedGameTime() const;
+			void SetElapsedGameTime(const std::chrono::milliseconds& elapsedGameTime);
+
+		private:
+			std::chrono::high_resolution_clock::time_point mCurrentTime;
+			std::chrono::milliseconds mTotalGameTime;
+			std::chrono::milliseconds mElapsedGameTime;
+		};
+	}
 }
